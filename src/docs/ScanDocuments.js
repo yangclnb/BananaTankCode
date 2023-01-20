03cbf60a9f184a061e4b74cb9e76de4fcefd1d15
banananboat <940868181@qq.com>
2023-01-20 20:52:48 +0800

    将编译的md添加到新的文件夹

--- src/docs/ScanDocuments.js
+++ src/docs/ScanDocuments.js
@@ -1,18 +1,33 @@
 const fs = require("fs");
 const fp = require("lodash/fp");
 
-// 转义md文件
+// 转义md文件的`
 const escapeString = fp.replace(/`/g, "\\`");
 
-// 获取要生成的JS文件内容
+// 拼接新的JS文件内容
 const getFileContent = fp.flowRight(
   (data) => `export const mdFile = \`${data}\``,
   escapeString,
   fs.readFileSync
 );
 
-// 获取要生成的JS文件名
-const getJSFileName = fp.replace(/.md/, ".js");
+// 为拆分为数组的文件路径添加新的文件夹
+const addDir = (dir) => {
+  return (arr) => {
+    const fileName = fp.last(arr);
+    arr[arr.length - 1] = dir;
+    arr.push(fileName);
+    return arr;
+  };
+};
+
+// 替换.md 为 .js
+const getJSFileName = fp.flowRight(
+  fp.join("/"),
+  addDir("jsDocs"),
+  fp.split("/"),
+  fp.replace(/.md/, ".js")
+);
 
 // 创建JS文件
 const createJSFile = fp.flowRight(
@@ -21,6 +36,11 @@ const createJSFile = fp.flowRight(
   }),
   fp.map((fileName) => "./src/docs/" + fileName),
   fp.filter((fileName) => fileName.endsWith(".md")),
+  (fileName) => {
+    // 若不存在 jsDocs 文件夹则添加此文件夹
+    if (!fileName.includes("jsDocs")) fs.mkdirSync(`${fileName}/jsDocs`);
+    return fileName;
+  },
   fs.readdirSync
 );
 
